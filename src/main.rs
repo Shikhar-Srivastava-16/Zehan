22a9ee8381e9a7dbe41f327901997f2ce84408ba
Shikhar Srivastava <shikhars2006@gmail.com>
2024-05-16 17:34:15 +0530

    added init method + framework for addition to 1D

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-// use Zehan::tools::array_tools;
+use std::vec;
+
+// use Zehan::tools::array_tools::help;
+use Zehan::tools::array_tools;
 
 fn main() {
-    Zehan::tools::array_tools::help();
+
+    let v1: Vec<i64> = vec![1, 2, 3];
+    let v2: Vec<i64> = vec![1, 2, 3];
+
+    let arr1: array_tools::Num_arr = array_tools::Num_arr{
+        len: 5,
+        vec: v1
+    };
+
+    let arr1: array_tools::Num_arr = array_tools::Num_arr{
+        len: 5,
+        vec: v2
+    };
+
 }
--- src/tools/array_tools.rs
+++ src/tools/array_tools.rs
@@ -1,3 +1,39 @@
+use std::vec;
+
 pub fn help() {
     println!("hello world")
+}
+pub struct Num_arr {
+    pub len: i32,
+    pub vec: Vec<i64>
+}
+
+
+
+impl Num_arr {
+    fn init_arr() -> Num_arr{
+        
+        let returner = Num_arr {
+            len: 0,
+            vec: Vec::new()
+        };
+
+        returner
+    }
+
+    fn add_array(self, arr_add: Num_arr){
+        // Num_arr
+
+        let mut index: usize = 0;
+        loop {
+            if index == Vec::len(&self.vec) {
+                break;
+            } else {
+                // add index of arr_add to index of self
+                index+=1;
+            }
+        }        
+
+        // res_arr
+    }
 }
